22490c5da7ea1c59d59ea48ad8df5894c4751b52
leehyosong <74747289+leehyosong@users.noreply.github.com>
2021-12-09 09:46:52 +0900

    Add files via upload

---
+++ src/main/java/com/example/mvoca_p/Search.java
@@ -0,0 +1,54 @@
+package com.example.mvoca_p;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class Search extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private TextView tvlogo;
+    private ImageView ivMenu;
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search);
+
+        tvlogo=findViewById(R.id.tv_logo);
+        ivMenu=findViewById(R.id.iv_menu);
+        drawerLayout=findViewById(R.id.drawer);
+        toolbar=findViewById(R.id.toolbar);
+
+
+        setSupportActionBar(toolbar);
+
+        ivMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClik : 클릭됨");
+                drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
+
+        tvlogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            }
+        });
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+}
